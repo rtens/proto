f0e25580868dd7804160538064a2ebd9534a7d68
Nikolas Martens <Nikolas.M@rtens.org>
2017-01-06 19:12:51 +0100

    Replace namespaces and reset git

--- setup.php
+++ setup.php
@@ -56,18 +56,31 @@ if (!file_exists($composerJson)) {
             ]
         ]
     ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
+    $replaceNamespaces = function($ins) use (&$replaceNamespaces, $vendor, $project) {
+        foreach ($ins as $in) {
+            if (is_dir($in)) {
+                $replaceNamespaces(glob($in . '/*'));
+            } else if (is_file($in) && substr($in, -4) == '.php') {
+                $contents = file_get_contents($in);
+                $contents = str_replace("vendor\\project", "$vendor\\$project", $contents);
+                file_put_contents($in, $contents);
+            }
+        }
+    };
+    $replaceNamespaces(['index.php', 'src', 'spec']);
+
+    echo "Installing dependencies (takes a while)" . PHP_EOL;
+    exec("php bin/composer.phar install 2>&1");
+
+    echo "Resetting git" . PHP_EOL;
+    exec("rm -rf .git; git init; git add .gitignore; git add *.*; git commit -m \"First commit\"");
+
+} else {
+
+    echo "Installing dependencies (takes a while)" . PHP_EOL;
+    exec("php bin/composer.phar install 2>&1");
 }
 
-echo "Installing dependencies (takes a while)" . PHP_EOL;
+echo "Done. Execute \"sh runspec.sh\" to run the tests and \"sh rundev.sh\" to run the application" . PHP_EOL;
 
-exec("php bin/composer.phar install");
-
-$replaceNamespaceIn = [
-    'index.php',
-    'src',
-    'spec'
-];
-
-// TODO replace vendor\sample with $vendor\$project
-// TODO Delete setup.php
-// TODO Reset .git
